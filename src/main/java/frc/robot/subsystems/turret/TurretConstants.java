9b54c1c24bbaa4dbfc314a402f2c41b0f0b9ef7c
MicahCoder <micahgruenwald@gmail.com>
2026-01-11 13:15:43 -0700

    Get Turret Translation

---
+++ src/main/java/frc/robot/subsystems/turret/TurretConstants.java
@@ -0,0 +1,12 @@
+package frc.robot.subsystems.turret;
+
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import frc.robot.util.motorUtil.MotorConfig;
+
+public class TurretConstants {
+
+  public static final MotorConfig kTurretMotorConfig = new MotorConfig("Turret").motorCan(13).p(0.0).d(0.0);
+  public static final Pose3d kTurretOffset = new Pose3d(-.15,-0.08,0.08, new Rotation3d());
+  
+}
