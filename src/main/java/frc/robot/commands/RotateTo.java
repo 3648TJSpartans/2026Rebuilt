0e8e3126accf556ad6c7ccb03a6e0486dade634c
Banks Malia <banksmalia1@proton.me>
2026-01-12 16:47:39 -0700

    Begin work on target servoing

---
+++ src/main/java/frc/robot/commands/RotateTo.java
@@ -0,0 +1,46 @@
+package frc.robot.commands;
+
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.commands.goToCommands.goToConstants;
+import frc.robot.subsystems.drive.Drive;
+import org.littletonrobotics.junction.Logger;
+
+public class RotateTo extends Command {
+  private final Drive drive;
+  private double targetRotation;
+
+  /**
+   * Constructor for RotateTo command.
+   *
+   * @param drive: A drive subsystem instance.
+   * @param targetRotation: The target rotation angle in radians.
+   */
+  public RotateTo(Drive drive, double targetRotation) {
+    this.drive = drive;
+    this.targetRotation = targetRotation;
+    addRequirements(drive);
+  }
+
+  @Override
+  public void execute() {
+    // Calculate the rotation displacement using our theta controller
+    double rotationDisplacement = goToConstants.thetaController.calculate(targetRotation);
+    ChassisSpeeds speeds = new ChassisSpeeds(0, 0, rotationDisplacement);
+    drive.runVelocity(speeds);
+
+    // I love logging
+    Logger.recordOutput("Commands/RotateTo/targetRotation", targetRotation);
+    Logger.recordOutput("Commands/RotateTo/rotationDisplacement", rotationDisplacement);
+  }
+
+  @Override
+  public boolean isFinished() {
+    return goToConstants.thetaController.atGoal();
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+    drive.stop();
+  }
+}
