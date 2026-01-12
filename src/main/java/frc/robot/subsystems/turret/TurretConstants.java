9f3ef1a6483d1e087f212f767ece642fbd228a4b
MicahCoder <micahgruenwald@gmail.com>
2026-01-12 07:08:09 -0700

    Update TurretConstants.java

--- src/main/java/frc/robot/subsystems/turret/TurretConstants.java
+++ src/main/java/frc/robot/subsystems/turret/TurretConstants.java
@@ -10,7 +10,7 @@ import frc.robot.util.motorUtil.MotorConfig;
 public class TurretConstants {
 
   public static final MotorConfig kTurretMotorConfig = new MotorConfig("Turret").motorCan(13).p(0.0).d(0.0).i(0.0).maxPower(0.1).minPower(0.1);
-  public static final Translation3d kTurretOffset = new Translation3d(.2,.2,.2);
+  public static final Translation3d kTurretOffset = new Translation3d(.2,-.3,.2);
   public static Rotation2d rotationOffset = new Rotation2d();
   public static double encoderPositionFactor = 1.0;
   
