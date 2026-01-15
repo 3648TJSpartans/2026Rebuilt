ee561dfe18899e936101ec74d4ce744ede1d28b2
cheickna <144643328+rakdosintolerant@users.noreply.github.com>
2026-01-15 13:57:55 -0700

    formatting and spelling errors

--- src/main/java/frc/robot/subsystems/climber/Climber.java
+++ src/main/java/frc/robot/subsystems/climber/Climber.java
@@ -1,56 +1,57 @@
 package frc.robot.subsystems.climber;
 
-import org.littletonrobotics.junction.Logger;
-
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.wpilibj.DigitalOutput;
 import frc.robot.util.motorUtil.RelEncoderSparkMax;
+import org.littletonrobotics.junction.Logger;
 
-public class Climber extends RelEncoderSparkMax{
-    private final DigitalOutput bottomSwitch;
-    private final DigitalOutput topSwitch;
+public class Climber extends RelEncoderSparkMax {
+  private final DigitalOutput bottomSwitch;
+  private final DigitalOutput topSwitch;
 
-    public Climber(){
-      super(ClimberConstants.motorConfig);
-      bottomSwitch = new DigitalOutput(ClimberConstants.bottomSwitchPort);
-      topSwitch = new DigitalOutput(ClimberConstants.topSwitchPort);
-    }
+  public Climber() {
+    super(ClimberConstants.motorConfig);
+    bottomSwitch = new DigitalOutput(ClimberConstants.bottomSwitchPort);
+    topSwitch = new DigitalOutput(ClimberConstants.topSwitchPort);
+  }
 
-    @Override
-    public void setPosition(double position){
-      position = MathUtil.clamp(position,ClimberConstants.minPosition.get(),ClimberConstants.maxPosition.get());
-      if(topSwitch.get()){
-        if(position>getPosition()){
-          super.setSpeed(0.0);
-          return;
-        }
+  @Override
+  public void setPosition(double position) {
+    position =
+        MathUtil.clamp(
+            position, ClimberConstants.minPosition.get(), ClimberConstants.maxPosition.get());
+    if (topSwitch.get()) {
+      if (position > getPosition()) {
+        super.setSpeed(0.0);
+        return;
       }
+    }
 
-      if(bottomSwitch.get()){
-        if(position<getPosition()){
-          super.setSpeed(0.0);
-          return;
-        }
+    if (bottomSwitch.get()) {
+      if (position < getPosition()) {
+        super.setSpeed(0.0);
+        return;
       }
-      super.setPosition(position);
     }
+    super.setPosition(position);
+  }
 
-    public Translation3d getTranslation(){
-      return new Translation3d(0,0,getPosition()*ClimberConstants.ecnoderPositionFactor);
-    }
+  public Translation3d getTranslation() {
+    return new Translation3d(0, 0, getPosition() * ClimberConstants.encoderPositionFactor);
+  }
 
-    public void setHeight(double height){
-      setPosition(height/ClimberConstants.ecnoderPositionFactor);
-    }
+  public void setHeight(double height) {
+    setPosition(height / ClimberConstants.encoderPositionFactor);
+  }
 
-    @Override
-    public void periodic() {
-      super.periodic();
-      updateInputs();
-    }
+  @Override
+  public void periodic() {
+    super.periodic();
+    updateInputs();
+  }
 
-    public void updateInputs(){
-      Logger.recordOutput("Subsystems/Climber/position", getTranslation());
-    }
+  public void updateInputs() {
+    Logger.recordOutput("Subsystems/Climber/position", getTranslation());
+  }
 }
--- src/main/java/frc/robot/subsystems/climber/ClimberConstants.java
+++ src/main/java/frc/robot/subsystems/climber/ClimberConstants.java
@@ -4,13 +4,22 @@ import frc.robot.util.TunableNumber;
 import frc.robot.util.motorUtil.MotorConfig;
 
 public class ClimberConstants {
-  public static final TunableNumber maxPosition = new TunableNumber("Subsystems/Climber/Limits/maxPosition", 1.0);
-  public static final TunableNumber minPosition = new TunableNumber("Subsystems/Climber/Limits/minPosiiton", 0.0);
+  public static final TunableNumber maxPosition =
+      new TunableNumber("Subsystems/Climber/Limits/maxPosition", 1.0);
+  public static final TunableNumber minPosition =
+      new TunableNumber("Subsystems/Climber/Limits/minPosiiton", 0.0);
 
-  public static final MotorConfig motorConfig = new MotorConfig("Subsystems/Climber/MotorIO").motorCan(16).p(0).i(0).d(0).maxPower(.1).minPower(-.1).positionTolerance(0.0);
+  public static final MotorConfig motorConfig =
+      new MotorConfig("Subsystems/Climber/MotorIO")
+          .motorCan(16)
+          .p(0)
+          .i(0)
+          .d(0)
+          .maxPower(.1)
+          .minPower(-.1)
+          .positionTolerance(0.0);
 
-  public static final double ecnoderPositionFactor = 1.0;
-
+  public static final double encoderPositionFactor = 1.0;
 
   public static final int bottomSwitchPort = 2;
   public static final int topSwitchPort = 3;
