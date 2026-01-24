f4b0b4ed1d72d0a4aeb687d170f3ce9ec88ca64b
Eric Krause <eric.krause@gmail.com>
2026-01-24 11:30:02 -0700

    Upgrade to 2026 libs, update deps.  Commented out BuildConstants logic

--- src/main/java/frc/robot/util/motorUtil/RelEncoderSparkMax.java
+++ src/main/java/frc/robot/util/motorUtil/RelEncoderSparkMax.java
@@ -2,6 +2,7 @@ package frc.robot.util.motorUtil;
 
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.ClosedLoopSlot;
+import com.revrobotics.spark.FeedbackSensor;
 import com.revrobotics.spark.SparkBase.ControlType;
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
@@ -9,7 +10,6 @@ import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkClosedLoopController.ArbFFUnits;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
-import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
 import com.revrobotics.spark.config.SparkMaxConfig;
 
 public class RelEncoderSparkMax extends MotorIO {
