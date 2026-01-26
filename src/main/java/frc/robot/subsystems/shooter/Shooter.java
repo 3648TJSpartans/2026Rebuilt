a34508650a26c89b49da82e2f7de1f54cdd6d81f
MicahCoder <micahgruenwald@gmail.com>
2026-01-26 10:07:57 -0700

    Add follow motor

--- src/main/java/frc/robot/subsystems/shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -1,21 +1,25 @@
 package frc.robot.subsystems.shooter;
 
+import frc.robot.util.motorUtil.MultiMotorSubsystem;
+import frc.robot.util.motorUtil.RelEncoderFollower;
 import frc.robot.util.motorUtil.RelEncoderSparkMax;
 import org.littletonrobotics.junction.Logger;
 
 import edu.wpi.first.wpilibj.DigitalOutput;
 
-public class Shooter extends RelEncoderSparkMax {
-
+public class Shooter{
+  private final RelEncoderSparkMax leadMotor;
+  private final RelEncoderSparkMax followMotor;
   public Shooter() {
-    super(ShooterConstants.kMotorConfig);
+    leadMotor = new RelEncoderSparkMax(ShooterConstants.kMotorConfigLead);
+    followMotor = new RelEncoderFollower(ShooterConstants.kMotorConfigFollow, leadMotor);
   }
 
   public void shootVelocity(double velocity) {
     double rpmSetpoint = velocity * ShooterConstants.kShooterVelocityFactor.get();
     Logger.recordOutput("Subsystems/Shooter/shootVelocity/velocity", velocity);
     Logger.recordOutput("Subsystems/Shooter/shootVelocity/rpmSetpoint", rpmSetpoint);
-    runFFVelocity(rpmSetpoint);
+    leadMotor.runFFVelocity(rpmSetpoint);
   }
 
   
--- src/main/java/frc/robot/subsystems/shooter/ShooterConstants.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterConstants.java
@@ -4,11 +4,14 @@ import frc.robot.util.TunableNumber;
 import frc.robot.util.motorUtil.MotorConfig;
 
 public class ShooterConstants {
-  public static MotorConfig kMotorConfig =
+  public static MotorConfig kMotorConfigLead =
       new MotorConfig("Subsystems/Shooter/MotorIO").motorCan(13).Ks(0).Kv(0).speedTolerance(0.0);
+public static MotorConfig kMotorConfigFollow =
+      new MotorConfig("Subsystems/Shooter/MotorIO").motorCan(14).Ks(0).Kv(0).speedTolerance(0.0).isInverted(true);
   /*Used to convert from RPM to m/s the ball is shot. RPM*s/m
    * Example: If revving the shooter to 5000 rpm results in a ball velocity of 15 m/s, then the factor will be 5000/15 = 333.33 RPM*s/m
    */
+  
   public static final TunableNumber kShooterVelocityFactor =
       new TunableNumber("Subsystems/Shooter/VelocityFactor", 400);
 }
