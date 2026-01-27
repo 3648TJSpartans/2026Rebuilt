00512ca0c041058d198c7bf55840deb8dfc702b7
MicahCoder <micahgruenwald@gmail.com>
2026-01-27 10:25:44 -0700

    Add validity check to trajectory

--- src/main/java/frc/robot/util/trajectorySolver/Trajectory.java
+++ src/main/java/frc/robot/util/trajectorySolver/Trajectory.java
@@ -7,33 +7,59 @@ public class Trajectory {
   private final double m_turretAngle;
   private final double m_shooterSpeed;
   private final double m_hangTime;
-  
+  private final boolean m_isValid;
 
-  public Trajectory( double shooterAngle,double turretAngle, double shooterSpeed,double hangTime){
+  private Trajectory(
+      double shooterAngle,
+      double turretAngle,
+      double shooterSpeed,
+      double hangTime,
+      boolean isValid) {
     m_shooterAngle = shooterAngle;
     m_turretAngle = turretAngle;
     m_shooterSpeed = shooterSpeed;
     m_hangTime = hangTime;
+    m_isValid = isValid;
   }
 
-  public double getShooterAngle(){
+  public Trajectory(double shooterAngle, double turretAngle, double shooterSpeed, double hangTime) {
+    this(shooterAngle, turretAngle, shooterSpeed, hangTime, true);
+  }
+
+  public static Trajectory invalidTrajectory = new Trajectory(0, 0, 0, 0, false);
+
+  public double getShooterAngle() {
     return m_shooterAngle;
   }
-  public double getTurretAngle(){
+
+  public double getTurretAngle() {
     return m_turretAngle;
-  } 
-  public double getShooterSpeed(){
+  }
+
+  public double getShooterSpeed() {
     return m_shooterSpeed;
   }
-  public double getHangTime(){
+
+  public double getHangTime() {
     return m_hangTime;
   }
 
-  public Rotation2d getTurretRotation(){
+  public Rotation2d getTurretRotation() {
     return new Rotation2d(m_turretAngle);
   }
 
-  public String toString(){
-    return "Shooter Angle: "+m_shooterAngle+"\nTurret Angle: "+m_turretAngle+"\nShooter Speed: "+m_shooterSpeed+"\nHang Time: "+m_hangTime;
+  public boolean isValid() {
+    return m_isValid;
+  }
+
+  public String toString() {
+    return "Shooter Angle: "
+        + m_shooterAngle
+        + "\nTurret Angle: "
+        + m_turretAngle
+        + "\nShooter Speed: "
+        + m_shooterSpeed
+        + "\nHang Time: "
+        + m_hangTime;
   }
 }
