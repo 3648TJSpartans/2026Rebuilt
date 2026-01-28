92069f5bca26807494e222439244846a7ed361fc
Avery <144643328+rakdosintolerant@users.noreply.github.com>
2026-01-28 14:23:05 -0700

    updated intake commands

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -453,7 +453,7 @@ public class RobotContainer {
                 .onFalse(Commands.runOnce(() -> m_intake.setRollers(0)));
         m_driveController
                 .y()
-                .whileTrue(Commands.runOnce(() -> m_intake.setSolenoidAndRollerDown()))
+                .onTrue(Commands.runOnce(() -> m_intake.setSolenoidAndRollerDown()))
                 .onFalse(Commands.runOnce(() -> m_intake.setSolenoidAndRollerUp()));
     }
 
