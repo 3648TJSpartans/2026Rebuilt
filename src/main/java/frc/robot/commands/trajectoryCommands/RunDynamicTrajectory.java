a2046ee996e36d1e403f4f83e56856b4c7556693
MicahCoder <micahgruenwald@gmail.com>
2026-01-28 10:57:32 -0700

    Add better traj logging

--- src/main/java/frc/robot/commands/trajectoryCommands/RunDynamicTrajectory.java
+++ src/main/java/frc/robot/commands/trajectoryCommands/RunDynamicTrajectory.java
@@ -1,5 +1,6 @@
 package frc.robot.commands.trajectoryCommands;
 
+import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Translation3d;
 import frc.robot.subsystems.hood.Hood;
 import frc.robot.subsystems.shooter.Shooter;
@@ -12,14 +13,25 @@ import org.littletonrobotics.junction.Logger;
 public class RunDynamicTrajectory extends RunTrajectoryCmd {
 
   /*
-   * Runs a dynamic trajectory, given a turret, shooter, hood, and kicker. Requires knowledge of the target, if the robot is in range, and how much the robot is tilting.
+   * Runs a dynamic trajectory, given a turret, shooter, hood, and kicker.
+   * Requires knowledge of the target, if the robot is in range, and how much the
+   * robot is tilting.
+   * 
    * @param turret - Turret Subsystem
+   * 
    * @param shooter - Shooter Subsystem
+   * 
    * @param hood - Hood Subsystem
+   * 
    * @param kicker - Kicker Subsystem
+   * 
    * @param targetsupplier - Translation3d Supplier for the target (field space)
-   * @param inRangeSupplier - Boolean suppleir asking if the robot is in a valid field spot to shoot.
-   * @param robotTiltSupplier - Supplys robot tilt... if tilt is too much, don't shoot.
+   * 
+   * @param inRangeSupplier - Boolean suppleir asking if the robot is in a valid
+   * field spot to shoot.
+   * 
+   * @param robotTiltSupplier - Supplys robot tilt... if tilt is too much, don't
+   * shoot.
    */
   public RunDynamicTrajectory(
       Turret turret,
@@ -44,9 +56,14 @@ public class RunDynamicTrajectory extends RunTrajectoryCmd {
           Translation3d target = targetSupplier.get();
           Translation3d turretPose = turret.getTurretFieldPose().getTranslation();
           double[] turretVelocity = turret.getTurretTranslationalVelocity();
-          Trajectory traj =
-              TrajectoryCalc.dynamicTrajectory(
-                  turretPose, target, turretVelocity, overhangAspect.get(), overhangHeight.get());
+          Trajectory traj = TrajectoryCalc.dynamicTrajectory(
+              turretPose, target, turretVelocity, overhangAspect.get(), overhangHeight.get());
+          Logger.recordOutput("Commands/RunDynamicTrajectory/trajectory/hangTime", traj.getHangTime());
+          Logger.recordOutput("Commands/RunDynamicTrajectory/trajectory/shooterSpeed", traj.getShooterSpeed());
+          Logger.recordOutput("Commands/RunDynamicTrajectory/trajectory/shooterAngle", traj.getShooterAngle());
+          Logger.recordOutput("Commands/RunDynamicTrajectory/trajectory/turretRotation", traj.getTurretRotation());
+          Logger.recordOutput("Commands/RunDynamicTrajectory/trajectory/shooterRotaiton",
+              new Rotation3d(0, traj.getShooterAngle(), traj.getTurretAngle()));
           Logger.recordOutput(
               "Commands/RunDynamicTrajectory/interpolatedTrajectory",
               TrajectoryCalc.interpolateTrajectory(traj, turretVelocity, turretPose));
