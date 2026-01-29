bbdffb5e8b265de44675ea4d490fabc0ce21460d
Avery <144643328+rakdosintolerant@users.noreply.github.com>
2026-01-29 14:25:10 -0700

    delete example subsystems (again)

--- src/main/java/frc/robot/subsystems/examplePneumatic/PneumaticConstants.java
+++
@@ -1,7 +0,0 @@
-package frc.robot.subsystems.examplePneumatic;
-
-public class PneumaticConstants {
-
-  public static final int solenoidChannel1 = 3;
-  public static final int solenoidChannel2 = 4;
-}
