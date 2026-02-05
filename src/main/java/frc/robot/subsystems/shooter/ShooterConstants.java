e8ba31d50918d3791b4ca1fc8d77dccf3249c313
MicahCoder <micahgruenwald@gmail.com>
2026-02-05 07:34:01 -0700

    Update Kicker for Statusable Digital Input

--- src/main/java/frc/robot/subsystems/shooter/ShooterConstants.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterConstants.java
@@ -23,21 +23,17 @@ public class ShooterConstants {
   //   .isInverted(true);
 
   public static MotorConfig kKickerMotorConfig =
-      new MotorConfig("Subsystems/Shooter/KickerMotorIO")
-          .motorCan(12)
-          .Ks(0)
-          .Kv(0)
-          .speedTolerance(0.0);
+      new MotorConfig("Subsystems/Kicker/MotorIO").motorCan(12).Ks(0).Kv(0).speedTolerance(0.0);
 
-  public static int kickerIRSensorChannel = 3;
+  public static final int kickerIRSensorChannel = 3;
 
   /*Used to convert from RPM to m/s the ball is shot. RPM*s/m
    * Example: If revving the shooter to 5000 rpm results in a ball velocity of 15 m/s, then the factor will be 5000/15 = 333.33 RPM*s/m
    */
   public static final TunableNumber kShooterVelocityFactor =
-      new TunableNumber("Subsystems/Shooter/VelocityFactor", 200);
+      new TunableNumber("Subsystems/Kicker/VelocityFactor", 200);
   public static final TunableNumber kickerSpeed =
-      new TunableNumber("Subsystems/Shooter/kickerSpeed", 1.0);
+      new TunableNumber("Subsystems/Kicker/kickerSpeed", 1.0);
   public static final TunableNumber kickerSlowSpeed =
-      new TunableNumber("Subsystems/Shooter/kickerSpeed", 0);
+      new TunableNumber("Subsystems/Kicker/kickerSpeed", 0);
 }
