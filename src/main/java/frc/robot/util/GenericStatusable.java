b8d26b2cf329e51a0d98b7df40835a78fc7c5305
MicahCoder <micahgruenwald@gmail.com>
2026-02-05 07:46:07 -0700

    Refactor GenericStatusable

--- src/main/java/frc/robot/util/GenericStatusable.java
+++ src/main/java/frc/robot/util/GenericStatusable.java
@@ -4,27 +4,19 @@ import frc.robot.Constants.Status;
 import java.util.function.Supplier;
 
 public class GenericStatusable implements Statusable {
-  private final Supplier<Boolean> m_isGood;
+  private final Supplier<Status> m_statusSupplier;
   private final String m_name;
-private final Status m_falseStatus;
 
-  public GenericStatusable(Supplier<Boolean> isGood, String name, Status falseStatus) {
-    m_isGood = isGood;
+  public GenericStatusable(Supplier<Status> statusSupplier, String name) {
+    m_statusSupplier = statusSupplier;
     m_name = name;
-    m_falseStatus = falseStatus;
-
   }
-  public GenericStatusable(Supplier<Boolean> isGood, String name){
-    this(isGood, name, Status.ERROR);
-  }
-
-
 
   public String getName() {
     return m_name;
   }
 
   public Status getStatus() {
-    return m_isGood.get() ? Status.OK : m_falseStatus;
+    return m_statusSupplier.get();
   }
 }
