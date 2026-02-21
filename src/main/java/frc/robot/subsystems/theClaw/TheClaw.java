172c9d6bcb9942ad6d7f79747c46e071b4ca3e8f
Avery <144643328+rakdosintolerant@users.noreply.github.com>
2026-02-21 08:21:54 -0700

    added isHomed to claw

--- src/main/java/frc/robot/subsystems/theClaw/TheClaw.java
+++ src/main/java/frc/robot/subsystems/theClaw/TheClaw.java
@@ -2,35 +2,45 @@ package frc.robot.subsystems.theClaw;
 
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Translation3d;
-import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.DigitalOutput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import frc.robot.Constants.Mode;
 import frc.robot.Constants.Status;
 import frc.robot.util.motorUtil.SparkIO;
 import frc.robot.util.statusableUtils.Statusable;
 import frc.robot.util.statusableUtils.StatusableDigitalInput;
-
 import org.littletonrobotics.junction.Logger;
 
 public class TheClaw extends SubsystemBase implements Statusable {
   private final StatusableDigitalInput bottomSwitch;
   private final SparkIO m_motor;
+  private boolean isHomed;
 
   public TheClaw(SparkIO motor) {
     m_motor = motor;
-    bottomSwitch = new StatusableDigitalInput(TheClawstants.bottomSwitchPort,"Subsystems/Claw/limitSwitch");
+    isHomed = Constants.currentMode == Mode.SIM ? true : false;
+    bottomSwitch =
+        new StatusableDigitalInput(TheClawstants.bottomSwitchPort, "Subsystems/Claw/limitSwitch");
   }
 
   public void setPosition(double position) {
-    position =
-        MathUtil.clamp(position, TheClawstants.minPosition.get(), TheClawstants.maxPosition.get());
-    if (bottomSwitch.get()) {
-      if (position < m_motor.getPosition()) {
-        m_motor.setPower(0.0);
-        return;
+    if (isHomed) {
+      position =
+          MathUtil.clamp(
+              position, TheClawstants.minPosition.get(), TheClawstants.maxPosition.get());
+      if (bottomSwitch.get()) {
+        if (position < m_motor.getPosition()) {
+          m_motor.setPower(0.0);
+          return;
+        }
       }
+      m_motor.setPosition(position);
     }
-    m_motor.setPosition(position);
+  }
+
+  public void setZero() {
+    isHomed = true;
+    m_motor.setEncoder(TheClawstants.minPosition.get());
   }
 
   public Translation3d getTranslation() {
@@ -43,6 +53,10 @@ public class TheClaw extends SubsystemBase implements Statusable {
 
   @Override
   public void periodic() {
+    if (bottomSwitch.get()) {
+      setZero();
+    }
+
     m_motor.periodic();
     updateInputs();
   }
@@ -50,23 +64,27 @@ public class TheClaw extends SubsystemBase implements Statusable {
   public void updateInputs() {
     Logger.recordOutput("Subsystems/Claw/position", getTranslation());
     Logger.recordOutput("Subsystems/Claw/limitSwitch", bottomSwitch.get());
+    Logger.recordOutput("Subsystems/Claw/isHomed", isHomed);
   }
 
   @Override
   public Status getStatus() {
-    if(m_motor.getStatus() != Status.OK){
+    if (m_motor.getStatus() != Status.OK) {
       return m_motor.getStatus();
     }
     return bottomSwitch.getStatus();
   }
+
   public void setPower(double power) {
-    if(bottomSwitch.get()){
-      if(power>0.0){
-        m_motor.stop();
-        return;
+    if (isHomed) {
+      if (bottomSwitch.get()) {
+        if (power > 0.0) {
+          m_motor.stop();
+          return;
+        }
       }
+      m_motor.setPower(power);
     }
-    m_motor.setPower(power);
   }
 
   public void stop() {
