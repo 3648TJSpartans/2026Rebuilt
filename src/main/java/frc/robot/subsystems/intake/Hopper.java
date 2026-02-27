8d1f165e1525a99998ff5fe35b34dc3d4cf02495
Avery <144643328+rakdosintolerant@users.noreply.github.com>
2026-02-27 07:34:25 -0700

    added getCurrentToSpeed() and jammed()

--- src/main/java/frc/robot/subsystems/intake/Hopper.java
+++ src/main/java/frc/robot/subsystems/intake/Hopper.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems.intake;
 
 import frc.robot.Constants.Status;
+import frc.robot.subsystems.climber.ClimberConstants;
 import frc.robot.util.motorUtil.RelEncoderSparkMax;
 
 public class Hopper extends RelEncoderSparkMax {
@@ -12,5 +13,15 @@ public class Hopper extends RelEncoderSparkMax {
   @Override
   public Status getStatus() {
     return super.getStatus();
+  }
+
+  public double getCurrentToSpeed() {
+    return Math.abs(getSpeed()) > 100 && Math.abs(getCurrent()) > .05
+        ? Math.abs(getCurrent() / getSpeed())
+        : 0.0;
+  }
+
+  public boolean jammed() {
+    return getCurrentToSpeed() > ClimberConstants.currentToSpeedThreshold.get();
   }
 }
