4814cbdfdb461ff13af910228d83e9a584fbc5e3
Micah Gruenwald <123602038+MicahCoder@users.noreply.github.com>
2026-03-02 18:58:00 -0700

    Flop ports

--- src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
@@ -64,9 +64,9 @@ public class IntakeConstants {
   public static final TunableNumber currentThreshold =
       new TunableNumber("Subsystems/Intake/Hopper/currentThreshold", 0.5);
 
-  public static final int upSolenoidChannel = 2;
+  public static final int upSolenoidChannel = 3;
 
-  public static final int downSolenoidChannel = 3;
+  public static final int downSolenoidChannel = 2;
   public static final Translation3d intakeOffset = new Translation3d(0.27, 0.0, .22);
   public static final Rotation3d intakeDownRotation = new Rotation3d(0, Math.PI / 2, 0);
   public static final Rotation3d intakeUpRotation = new Rotation3d();
